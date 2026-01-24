4d7afe1ef3436f7a81eb3376920bb0994ca36dc9
krishna <krishnaptl0414@gmail.com>
2026-01-24 17:46:24 +0530

    Truthy or Falsy value Learn

---
+++ 04_controlflow/03_truthy_aur_falsy_value.js
@@ -0,0 +1,68 @@
+// const userEmail = "Krish@na.ai"
+
+// if (userEmail) {
+//     console.log("Got User Email");
+// }
+// else {
+//     console.log("Don't Have User Email ");
+// }
+
+
+
+/* 
+
+falsy value:
+
+false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+*/
+
+/*
+
+truthy value:
+
+"0",'false'," ",[],{},function(){}
+
+*/
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got User Email");
+}
+else {
+    console.log("Don't Have User Email ");
+}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is Empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is Empty");
+}
+
+
+/********************Nullish coalescing Operator(??): null undefined  ****************** */
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+console.log(val1);
+
+
+/***************************Terniary Operator ***************/
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+
+
+
+
+
