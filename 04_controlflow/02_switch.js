db72503237376ffa0347f37cf90d61dfe160662e
krishna <krishnaptl0414@gmail.com>
2026-01-24 15:13:44 +0530

    switch Statement completed

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,68 @@
+// ***********************Switch Case ***********
+
+/* 
+ SYNTAX :
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+} 
+*/
+
+/*  When number check
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+
+    default:
+        console.log("default Case Match");
+
+        break;
+}
+
+*/
+
+/*
+ break :
+   when one point or case condition true its don't check another case & don't print another case  
+   
+*/const month = "April"
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "Febuary":
+        console.log("Febuary");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+
+    default:
+        console.log("default Case Match");
+
+        break;
+}
+
